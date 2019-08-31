51c569f379a833e5f7469bac99269378141b5bcd
Michael Kitzan <mkitzan45@gmail.com>
2019-08-31 13:57:07 -0700

    Peanut brain scope bug

--- test/file_transfer/file_transfer_client.cpp
+++ test/file_transfer/file_transfer_client.cpp
@@ -17,7 +17,7 @@ int main() {
 	for(auto peer : devices) {
 		try {
 			cout << "Creating client socket to " << peer << endl << flush;
-			unique_socket us(bluegrass::socket<RFCOMM>(peer, 0x1));
+			unique_socket us(bluegrass::socket<L2CAP>(peer, 0x1001));
 			cout << "Client construction succeeded" << endl << flush;
 			
 			while(us.receive(&packet)) {
--- test/file_transfer/file_transfer_server.cpp
+++ test/file_transfer/file_transfer_server.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 using namespace bluegrass;
 
-void transfer(bluegrass::socket<RFCOMM>& conn) {	
+void transfer(bluegrass::socket<L2CAP>& conn) {	
 	unique_socket us(std::move(conn));
 	
-	bdaddr_t peer{ us.sockaddr().addr.rc_bdaddr };
+	bdaddr_t peer{ us.sockaddr().addr.l2_bdaddr };
 	struct packet_t packet{ 0, 0 };
 	uint8_t count{ 1 };
 	
@@ -40,18 +40,17 @@ void transfer(bluegrass::socket<RFCOMM>& conn) {
 
 int main() {
 	cout << "Creating service_queue" << endl << flush;
-	service_queue<bluegrass::socket<RFCOMM>, ENQUEUE> sq(transfer, 4, 2);
+	service_queue<bluegrass::socket<L2CAP>, ENQUEUE> sq(transfer, 4, 2);
 	
 	try {
 		cout << "Creating server" << endl << flush;
-		server<RFCOMM> s(&sq, 0x1, 4);
+		server<L2CAP> s(&sq, 0x1001, 4);
+		cout << "Server construction succeeded" << endl << flush;
+		for(;;);
 	} catch(...) {
 		cout << "Server construction failed" << endl;
 		return 0;
 	}
-	
-	cout << "Server construction succeeded" << endl << flush;
-	for(;;);
 	
 	return 0;
 }
